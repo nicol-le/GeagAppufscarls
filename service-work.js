7d0aed2443656509dc8791d310db6477c93c540f
nicol-le <gomesnocolle@gmail.com>
2024-12-12 15:08:40 -0300

    Update service-work.js

--- service-work.js
+++ service-work.js
@@ -1,19 +1,20 @@
 self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open('v1').then((cache) => {
-            return cache.addAll([
-                '/',
-                '/index.html',
-            
-            ]);
-        })
-    );
+  event.waitUntil(
+    caches.open('meu-cache').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/style.css',
+        '/app.js',
+      ]);
+    })
+  );
 });
 
 self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request).then((cachedResponse) => {
-            return cachedResponse || fetch(event.request);
-        })
-    );
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
 });
